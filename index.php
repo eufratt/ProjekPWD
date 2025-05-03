5bf51cfc7252c5f075cc33a5bc80b98872994ad6
eufratt <ayyasheufrat2@gmail.com>
2025-05-03 17:43:07 +0700

    tambah judul website

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <img src="asset/makanan2x.png" class = "bg-image">
+    <div class="position-absolute top-50 start-50 translate-middle text-center konten">
+      <h2 class="mt-2">Makanan Khas Jawa</h2>
+      <div class="mt-4 mb-2">
+        <a href="login.php" class="btn btn-primary ms-3">Jelajahi</a>
+      </div>
+    </div>
 </body>
 </html>
