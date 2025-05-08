6268119594706a08900c2790fe9200a9b5540a03
WildaGhoniyuJiddan <ezajidan@gmail.com>
2025-05-08 16:38:41 +0700

    edit tampilan login

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body>
     <img src="asset/makanan2x.png" class = "bg-image">
     <div class="position-absolute top-50 start-50 translate-middle text-center konten">
-      <h2 class="mt-2">Makanan Khas Jawa</h2>
+      <h2 class="mt-2">Kuliner Khas Jawa</h2>
       <div class="mt-4 mb-2">
         <a href="login.php" class="btn btn-primary ms-3">Jelajahi</a>
       </div>
